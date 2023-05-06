211e4dbf65e3157950f5ac84c751d4c0465a1a20
homa39 <100782554+homa39@users.noreply.github.com>
2023-05-06 20:43:14 +0300

    Main Version 0.1

---
+++ bin/main/app/forms/MainForm.php
@@ -0,0 +1,52 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event showing 
+     */
+    function doShowing(UXWindowEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button4.action 
+     */
+    function doButton4Action(UXEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button6.action 
+     */
+    function doButton6Action(UXEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button7.action 
+     */
+    function doButton7Action(UXEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
